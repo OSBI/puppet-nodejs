546b557f5e968ccb3b2ae245752daa9b383e1c58
Tom Barber <tom@analytical-labs.com>
2011-10-20 15:43:41 +0100

    node

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,16 @@ class nodejs {
                 content => "deb http://ppa.launchpad.net/gias-kay-lee/npm/ubuntu natty main"
         }
 
+		apt::key { "FD4B2702":
+			ensure => present,
+		}
+		
         apt::sources_list { "node":
                 content => "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu natty main"
+        }
+        
+        apt::key { "C7917B12":
+        	ensure => present
         }
                 
         package { "npm" :
