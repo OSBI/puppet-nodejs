71e0f4e5ce330d790f5631dcb7d2d2ef9717c7af
Tom Barber <tom@analytical-labs.com>
2011-10-20 14:58:00 +0100

    node

---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+class node {
+
+        apt::sources_list { "npm":
+                content => "deb http://ppa.launchpad.net/gias-kay-lee/npm/ubuntu natty main"
+        }
+
+        apt::sources_list { "node":
+                content => "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu natty main"
+        }
+                
+        package { "npm" :
+        	ensure => present,        
+        	require => apt::Sources_list["npm"],
+        }
+
+		package { "nodejs" :
+        	ensure => present,
+        	require => Apt::Sources_list["node"],        
+        }
+}
